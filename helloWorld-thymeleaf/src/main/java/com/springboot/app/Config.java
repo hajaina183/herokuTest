e30536072f75ad2fdd3cc7d27b16ef50399cc80b
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2022-02-22 22:12:20 +0300

    ping

---
+++ helloWorld-thymeleaf/src/main/java/com/springboot/app/Config.java
@@ -0,0 +1,19 @@
+package com.springboot.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableScheduling
+public class Config {
+	private final RestTemplate restTemplate = new RestTemplate();
+
+    @Scheduled(fixedDelay = 30 * 60 * 1000)
+    public void keepAwake() {
+        String pingUrl = "https://hajaina-11.herokuapp.com/ping";
+        String result = restTemplate.getForObject(pingUrl, String.class);
+        System.out.println(result);
+    }
+}
--- helloWorld-thymeleaf/src/main/java/com/springboot/app/HelloWorldThymeleafApplication.java
+++ helloWorld-thymeleaf/src/main/java/com/springboot/app/HelloWorldThymeleafApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldThymeleafApplication {
 
 	public static void main(String[] args) {
@@ -24,5 +27,10 @@ public class HelloWorldThymeleafApplication {
             .setCachePeriod(0);
         }
     }
+	
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
+	}
 
 }
